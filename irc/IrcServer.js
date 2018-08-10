5ea8ae20ce9dd7077aebe03f06fca8cf016859eb
Claus Joergensen <claus.joergensen@outlook.com>
2018-08-10 13:02:17 +0200

    API docs for IrcServer

--- irc/IrcServer.js
+++ irc/IrcServer.js
@@ -5,10 +5,39 @@ const util = require('util')
 const events = require('events')
 const { EventEmitter } = events
 
+/**
+ * @class IrcServer
+ * @extends EventEmitter
+ *
+ * Represents an IRC server that exists on a specific IrcClient.
+ */
 module.exports = class IrcServer extends EventEmitter {
 
+  /*
+   * Constructs a new IrcServer for a given hostName.
+   *
+   * @access internal
+   * @constructor
+   * @param {String} hostName The server host name.
+  */
   constructor (hostName) {
     super()
-    this.hostName = hostName
+    this._hostName = hostName
+  }
+
+  /**
+   * Gets the host name of the server.
+   */
+  get hostName() {
+    return this._hostName
+  }
+
+  /**
+   * Returns a string representation of this instance.
+   *
+   * @return {String} A string that represents this instance.
+   */
+  toString () {
+    return this._hostName
   }
 }
