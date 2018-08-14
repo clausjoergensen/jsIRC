4da43e67422826548a3dac3696d1c4926d493bec
Claus Joergensen <claus.joergensen@outlook.com>
2018-08-14 03:29:24 +0200

    Improved package documentation for the thrown Errors

--- src/Errors.js
+++ src/Errors.js
@@ -17,28 +17,112 @@ class ExtendableError extends Error {
 }
 
 /**
- * @private
+ * The error that is thrown when null is passed to a function that does not accept it as a valid argument.
+ *
+ * @package
+ * @extends Error
  */
 class ArgumentNullError extends ExtendableError {
-  constructor (arg) {
-    super(`Argument '${arg}' is null.`)
+  /**
+   * A human-readable description of the error.
+   * @hideconstructor
+   * @param {String} paramName The name of the parameter that caused the error.
+   */
+  constructor (paramName) {
+    super(`Argument '${paramName}' is null.`)
+  }
+
+  /** Error message. */
+  get message () {
+    return super.message
+  }
+
+  /** Error name. */
+  get name () {
+    return super.name
   }
 }
 
 /**
- * @private
+ * The error that is thrown when one of the arguments provided to a function is not valid.
+ *
+ * @package
+ * @extends Error
  */
-class ArgumentError extends ExtendableError {}
+class ArgumentError extends ExtendableError {
+  /**
+   * A human-readable description of the error.
+   * @hideconstructor
+   * @param {String} message A human-readable description of the error.
+   */
+  constructor (message) { // eslint-disable-line no-useless-constructor
+    super(message)
+  }
+
+  /** Error message. */
+  get message () {
+    return super.message
+  }
+
+  /** Error name. */
+  get name () {
+    return super.name
+  }
+}
 
 /**
- * @private
+ * The error that is thrown when a function call is invalid for the object's current state.
+ *
+ * @package
+ * @extends Error
  */
-class InvalidOperationError extends ExtendableError {}
+class InvalidOperationError extends ExtendableError {
+  /**
+   * A human-readable description of the error.
+   * @hideconstructor
+   * @param {String} message A human-readable description of the error.
+   */
+  constructor (message) { // eslint-disable-line no-useless-constructor
+    super(message)
+  }
+
+  /** Error message. */
+  get message () {
+    return super.message
+  }
+
+  /** Error name. */
+  get name () {
+    return super.name
+  }
+}
 
 /**
- * @private
+ * The error that is thrown when an error is made while using a network protocol.
+ *
+ * @package
+ * @extends Error
  */
-class ProtocolViolationError extends ExtendableError {}
+class ProtocolViolationError extends ExtendableError {
+  /**
+   * A human-readable description of the error.
+   * @hideconstructor
+   * @param {String} message A human-readable description of the error.
+   */
+  constructor (message) { // eslint-disable-line no-useless-constructor
+    super(message)
+  }
+
+  /** Error message. */
+  get message () {
+    return super.message
+  }
+
+  /** Error name. */
+  get name () {
+    return super.name
+  }
+}
 
 module.exports = {
   ArgumentError: ArgumentError,
