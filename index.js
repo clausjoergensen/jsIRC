03db2f0ab4cda691d0b3f9d1b83c58890c7ae4c1
Claus Joergensen <claus.joergensen@outlook.com>
2018-08-15 19:41:56 +0200

    Export all client objects, so the client can compare them against message sources in events

--- index.js
+++ index.js
@@ -3,10 +3,23 @@
 
 const IrcClient = require('./src/IrcClient.js')
 const IrcFloodPreventer = require('./src/IrcFloodPreventer.js')
+const IrcUser = require('./src/IrcUser.js')
+const IrcLocalUser = require('./src/IrcLocalUser.js')
+const IrcServer = require('./src/IrcServer.js')
+const IrcChannel = require('./src/IrcChannel.js')
+const IrcChannelUser = require('./src/IrcChannelUser.js')
+const IrcChannelType = require('./src/IrcChannelType.js')
+
 const CtcpClient = require('./src/CtcpClient.js')
 
 module.exports = {
   IrcClient: IrcClient,
   IrcFloodPreventer: IrcFloodPreventer,
+  IrcUser: IrcUser,
+  IrcLocalUser: IrcLocalUser,
+  IrcServer: IrcServer,
+  IrcChannel: IrcChannel,
+  IrcChannelUser: IrcChannelUser,
+  IrcChannelType: IrcChannelType,
   CtcpClient: CtcpClient
 }
